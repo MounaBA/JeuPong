b71518cde468a47f91d75a1251a6a58d68aadf58
Mouna Ben Ameur <59797918+MounaBA@users.noreply.github.com>
2020-05-27 23:42:48 +0200

    Update Jeu.java

--- monPong/src/mon/pong/Jeu.java
+++ monPong/src/mon/pong/Jeu.java
@@ -10,16 +10,10 @@ import static mon.pong.Action.vies;
 public class Jeu {
 
     public static void main(String[] args) {
-        /*FileText f = new FileText();
-        String a;
-        int b;
-        a = f.continuer();
-            b = Integer.parseInt(a);
-            niveau = (b / 10);
-            vies = (b % 10); */
+        
         Action action = new Action();
         action.init();
-        action.parametresBalle();
+        //action.parametresBalle();
         //action.parametresballe(niveau, vies);
     }
 }
